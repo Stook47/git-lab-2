a70246bfe1b0f22003f7f4fecfb5baf33b0020ef
Joshua Stookey <js770518@ohio.edu>
2020-01-22 19:24:23 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joshua Stookey
+ *        Date: 1/22/2020
+ * Description: Hello Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
